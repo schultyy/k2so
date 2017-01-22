36a2a9bcaa8820ee7ad40f2c19cbbfb5aeb1f52f
Jan Schulte <hello@unexpected-co.de>
2017-01-22 16:25:47 +0100

    feat: Integrate clap skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const APP_NAME: &'static str = "K2-SO -- Deployment Droid 🤖✨";
+
 fn main() {
-    println!("K2-SO -- Deployment Droid 🤖✨");
+    let matches = App::new(APP_NAME)
+            .version(VERSION)
+            .author("Jan Schulte <jan@unexpected-co.de>")
+            .about("Deploys your tool -- The captain said I have to")
+            .get_matches();
 }
