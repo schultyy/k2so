0b3da09faef12b497b00dfa219eb240701d16589
Jan Schulte <hello@unexpected-co.de>
2017-01-22 17:23:57 +0100

    feat: add commands for address and role

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,19 @@ fn main() {
             .version(VERSION)
             .author("Jan Schulte <jan@unexpected-co.de>")
             .about("Deploys your tool -- The captain said I have to")
+            .arg(Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("IP")
+                .help("Define an address"))
+            .arg(Arg::with_name("role")
+                .short("r")
+                .long("role")
+                .value_name("ROLE")
+                .help("Define a role"))
             .get_matches();
+    let ip = matches.value_of("address").unwrap();
+    let role = matches.value_of("role").unwrap();
+    println!("Value for config: {}", ip);
+    println!("Value for config: {}", role);
 }
