e865802884cac3eaf477913b08161c146c2b2d2b
Jan Schulte <jan.schulte@asquera.de>
2017-01-22 21:48:28 +0100

    feat: add 'add' subcommand
    This allows to define a mapping between IP address and a role

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,18 @@ fn main() {
             .version(VERSION)
             .author("Jan Schulte <jan@unexpected-co.de>")
             .about("Deploys your tool -- The captain said I have to")
-            .arg(Arg::with_name("address")
-                .short("a")
-                .long("address")
-                .value_name("IP")
-                .help("Define an address"))
-            .arg(Arg::with_name("role")
-                .short("r")
-                .long("role")
-                .value_name("ROLE")
-                .help("Define a role"))
-            .get_matches();
-    let ip = matches.value_of("address").unwrap();
-    let role = matches.value_of("role").unwrap();
-    println!("Value for config: {}", ip);
-    println!("Value for config: {}", role);
+            .subcommand(SubCommand::with_name("add")
+              .arg(Arg::with_name("address")
+                  .index(1)
+                  .required(true)
+                  .requires("role")
+                  .help("Define an address"))
+              .arg(Arg::with_name("role")
+                  .index(2)
+                  .help("Define a role"))
+            ).get_matches();
+    if let Some(ref matches) = matches.subcommand_matches("add") {
+      println!("Role: {}", matches.value_of("role").unwrap());
+      println!("Address: {}", matches.value_of("address").unwrap());
+    }
 }
