c1bc554a85b8da5716595b9af758bf259bf66cb3
Jan Schulte <hello@unexpected-co.de>
2017-02-02 21:10:04 +0100

    feat: Read server file
    If it's not existent then we use a default config

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,26 @@
+#[derive(Default)]
+#[derive(Debug)]
 #[derive(RustcEncodable)]
+#[derive(RustcDecodable)]
 pub struct Config {
   pub roles: Vec<Role>
 }
 
+#[derive(Default)]
+#[derive(Debug)]
 #[derive(RustcEncodable)]
+#[derive(RustcDecodable)]
 pub struct Role {
   pub name: String,
   pub address: String
 }
 
 impl Config {
-  pub fn new(role: String, address: String) -> Config {
+  pub fn new() -> Config {
+    Default::default()
+  }
+
+  pub fn new_with_role(role: String, address: String) -> Config {
     Config {
       roles: vec!(Role{
         name: role,
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,53 @@ extern crate toml;
 extern crate rustc_serialize;
 mod config;
 use clap::{Arg, App, SubCommand};
+use std::io::prelude::*;
+use std::fs::File;
+use toml::{Parser, Value};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const APP_NAME: &'static str = "K2-SO -- Deployment Droid 🤖✨";
 
-fn add_to_file(role: String, address: String) {
-    let config = config::Config::new(role, address);
-    let result = toml::encode_str(&config);
+fn read_server_file() -> config::Config {
+    let mut config_toml = String::new();
+    let path = "servers.toml";
 
-    println!("{:?}", result);
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(_)  => {
+            return config::Config::new();
+        }
+    };
+
+    file.read_to_string(&mut config_toml)
+            .unwrap_or_else(|err| panic!("Error while reading config: [{}]", err));
+
+    let mut parser = Parser::new(&config_toml);
+    let toml = parser.parse();
+
+    if toml.is_none() {
+        for err in &parser.errors {
+            let (loline, locol) = parser.to_linecol(err.lo);
+            let (hiline, hicol) = parser.to_linecol(err.hi);
+            println!("{}:{}:{}-{}:{} error: {}",
+                     path, loline, locol, hiline, hicol, err.desc);
+        }
+        panic!("Exiting server");
+    }
+
+    let config = Value::Table(toml.unwrap());
+    match toml::decode(config) {
+        Some(t) => t,
+        None => panic!("Error while deserializing config")
+    }
+}
+
+fn add_to_file(role: String, address: String) {
+    let config = config::Config::new_with_role(role, address);
+    let toml_string = toml::encode_str(&config);
+
+    let mut file = std::fs::File::create("servers.toml").unwrap();
+    file.write_all(toml_string.as_bytes()).expect("Could not write to file!");
 }
 
 fn main() {
